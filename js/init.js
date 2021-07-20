db9f925c5cc60e313758351357b47e8fd7d528a2
RuthVianey <87659314+RuthVianey@users.noreply.github.com>
2021-07-20 13:19:32 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAhO3TjKnprLCFuR36ljloJ2wTKlauTuOw",
+    authDomain: "chat801rpz.firebaseapp.com",
+    projectId: "chat801rpz",
+    storageBucket: "chat801rpz.appspot.com",
+    messagingSenderId: "254059076232",
+    appId: "1:254059076232:web:4e79b23a70f6ae0acc1d3d",
+    measurementId: "G-FH9BWZBVJF"
+});
